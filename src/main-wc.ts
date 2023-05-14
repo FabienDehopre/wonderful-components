f8c4c271f611e7e47df11cc15ccfa6f796f6d316
Fabien Dehopré <fabien@dehopre.com>
2023-05-14 21:36:16 +0200

    implement night & day button

--- src/main-wc.ts
+++ src/main-wc.ts
@@ -1,6 +1,7 @@
 import {createApplication} from "@angular/platform-browser";
 import {createCustomElement} from "@angular/elements";
 import {GalaxyButtonComponent} from "./app/galaxy-button/galaxy-button.component";
+import {NightDayToggleComponent} from "./app/night-day-toggle/night-day-toggle.component";
 
 (async () => {
   const app = await createApplication({
@@ -9,5 +10,7 @@ import {GalaxyButtonComponent} from "./app/galaxy-button/galaxy-button.component
   const galaxyButtonElement = createCustomElement(GalaxyButtonComponent, {
     injector: app.injector,
   });
+  const nightDayToggleElement = createCustomElement(NightDayToggleComponent, { injector: app.injector });
   customElements.define('wc-galaxy-button', galaxyButtonElement);
+  customElements.define('wc-night-day-toggle', nightDayToggleElement);
 })();
